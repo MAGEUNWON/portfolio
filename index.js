9e8eefed73617a2f99828fbfdf0204a109b4120a
mageunwon <geunwon1947@gmail.com>
2023-01-01 22:58:00 +0900

    포트폴리오 변경중

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var root = document.getElementById('root');
 var planet = root === null || root === void 0 ? void 0 : root.querySelector('div');
 // console.log(planet)
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  document.body.addEventListener("click", e=>{
+    if(e.target.matches("[data-link")){
+      e.preventDefault();
+      navigateTo(e.target.href)
+    }
+  });
+  router();
+})
--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-window.addEventListener("DOMContentLoaded", (e) => {
-  new App("#root");
-});
+// window.addEventListener("DOMContentLoaded", (e) => {
+//   new App("#root");
+// });
