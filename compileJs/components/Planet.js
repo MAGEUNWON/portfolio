08f38912f599a0b377a45f6d42d3ecf0ff181a02
unknown <geunwon1947@gmail.com>
2023-01-05 14:25:56 +0900

    슬라이더 완성

--- compileJs/components/Planet.js
+++ compileJs/components/Planet.js
@@ -4,35 +4,53 @@ var slider = document.getElementById("slider");
 var ul = document.querySelector("ul");
 // console.log(ul)
 var li = document.querySelectorAll("li");
-// console.log(li)
+console.dir(li);
+var lastLi = li.length;
+console.dir(lastLi);
 var nextButton = document.getElementById("nextButton");
 var prevButton = document.getElementById("prevButton");
 //typescript 디버거 tasks.json 빌드 한글로 되있어서 안됬음
 var liWidth = li[0].offsetWidth;
-// console.log(li[0]);
-console.log(liWidth);
+// console.log(li.length);
+// console.dir(li.length - 1);
 var totalWidth = li.length * liWidth;
 ul.style.width = "".concat(totalWidth, "px");
 console.log(totalWidth);
 var current = 1;
-// function slideCount():any=>{
-//   current++;
-//   let moving: number | string = current * liWidth - liWidth;
-//   ul.style.transform = `translateX(${moving}px)`;
-// }
+// let current = 1;
 var NslideCount = function () {
     current++;
+    console.log(current);
     var moving = current * liWidth - liWidth;
-    ul.style.transform = "translateX(-".concat(moving, "px)");
+    if (current <= lastLi) {
+        ul.style.transform = "translateX(-".concat(moving, "px)");
+    }
+    else {
+        current = 0;
+        ul.style.transform = "translateX(-".concat(moving, "px)");
+    }
+    // ul.style.transform = `translateX(-${moving}px)`;
+    // console.log((ul.style.transform = `translateX(-${moving}px)`));
+    // if (current === lastLi) {
+    //   ul.style.transform = `translateX(-0px)`;
+    // }
 };
 var PslideCount = function () {
-    current++;
-    var moving = current * liWidth - liWidth;
-    ul.style.transform = "translateX(".concat(moving, "px)");
+    current--;
+    // let moving = current * liWidth - liWidth;
+    console.log(current);
+    if (current <= 0) {
+        current = lastLi;
+    }
+    var backmoving = current * liWidth - liWidth;
+    ul.style.transform = "translateX(-".concat(backmoving, "px)");
+    // ul.insertAdjacentHTML("afterbegin", li[3]);
+    // ul.style.transform = `translateX(-${moving}px)`;
+    // console.log((ul.style.transform = `translateX(-${moving}px)`));
 };
-// prevButton.addEventListener("click", () => {
-//   PslideCount();
-// });
+prevButton.addEventListener("click", function () {
+    PslideCount();
+});
 nextButton.addEventListener("click", function () {
     NslideCount();
 });
--- static/ts/components/Planet.ts
+++ static/ts/components/Planet.ts
@@ -3,15 +3,17 @@ const slider = document.getElementById("slider");
 const ul: HTMLUListElement | any = document.querySelector("ul");
 // console.log(ul)
 const li = document.querySelectorAll("li");
-// console.log(li)
+console.dir(li);
+const lastLi: any = li.length;
+console.dir(lastLi);
 const nextButton: HTMLElement | any = document.getElementById("nextButton");
 
 const prevButton: HTMLElement | any = document.getElementById("prevButton");
 //typescript 디버거 tasks.json 빌드 한글로 되있어서 안됬음
 
 const liWidth = li[0].offsetWidth;
-// console.log(li[0]);
-console.log(liWidth);
+// console.log(li.length);
+// console.dir(li.length - 1);
 
 const totalWidth = li.length * liWidth;
 ul.style.width = `${totalWidth}px`;
@@ -19,28 +21,44 @@ ul.style.width = `${totalWidth}px`;
 console.log(totalWidth);
 
 let current: number = 1;
-
-// function slideCount():any=>{
-//   current++;
-//   let moving: number | string = current * liWidth - liWidth;
-//   ul.style.transform = `translateX(${moving}px)`;
-// }
+// let current = 1;
 
 const NslideCount = () => {
   current++;
+  console.log(current);
   let moving = current * liWidth - liWidth;
-  ul.style.transform = `translateX(-${moving}px)`;
+  if (current <= lastLi) {
+    ul.style.transform = `translateX(-${moving}px)`;
+  } else {
+    current = 0;
+    ul.style.transform = `translateX(-${moving}px)`;
+  }
+
+  // ul.style.transform = `translateX(-${moving}px)`;
+  // console.log((ul.style.transform = `translateX(-${moving}px)`));
+  // if (current === lastLi) {
+  //   ul.style.transform = `translateX(-0px)`;
+  // }
 };
 
 const PslideCount = () => {
-  current++;
-  let moving = current * liWidth - liWidth;
-  ul.style.transform = `translateX(${moving}px)`;
+  current--;
+  // let moving = current * liWidth - liWidth;
+  console.log(current);
+  if (current <= 0) {
+    current = lastLi;
+  }
+  let backmoving = current * liWidth - liWidth;
+  ul.style.transform = `translateX(-${backmoving}px)`;
+  // ul.insertAdjacentHTML("afterbegin", li[3]);
+  // ul.style.transform = `translateX(-${moving}px)`;
+  // console.log((ul.style.transform = `translateX(-${moving}px)`));
 };
 
-// prevButton.addEventListener("click", () => {
-//   PslideCount();
-// });
+prevButton.addEventListener("click", () => {
+  PslideCount();
+});
+
 nextButton.addEventListener("click", () => {
   NslideCount();
 });
