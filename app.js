c7109d5a1ec3a2baf35d0fc2665ca55697c34bfd
mageunwon <geunwon1947@gmail.com>
2023-01-12 17:59:00 +0900

    영화 추천 사이트 설명 페이지 완성

--- app.js
+++ app.js
@@ -7,20 +7,23 @@ var http = require("http");
 var server = http.createServer(function (req, res) {
     var method = req.method;
     var _url = req.url;
-    // url 변수로 담아서 switch문 매개변수로 씀. 
+    // url 변수로 담아서 switch문 매개변수로 씀.
     var File = {
-        main: fs.readFileSync("public/index.html"),
-        Planet: fs.readFileSync("static/html/Planet.html"),
-        indexCss: fs.readFileSync("static/css/index.css"),
-        PlanetCss: fs.readFileSync("static/css/Planet.css"),
-        PlanetImg: fs.readFileSync("static/img/geunwonPlanet.png"),
-        universeImg: fs.readFileSync("static/img/universeBg.png"),
-        PlanetBg: fs.readFileSync("static/img/PlanetBg.png"),
-        HillBg: fs.readFileSync("static/img/HillBg.jpg"),
-        indexJs: fs.readFileSync("compileJs/index.js"),
-        PlanetJs: fs.readFileSync("compileJs/components/Planet.js")
+        main: fs.readFileSync('public/index.html'),
+        Planet: fs.readFileSync('static/html/Planet.html'),
+        indexCss: fs.readFileSync('static/css/index.css'),
+        PlanetCss: fs.readFileSync('static/css/Planet.css'),
+        PlanetImg: fs.readFileSync('static/img/geunwonPlanet.png'),
+        universeImg: fs.readFileSync('static/img/universeBg.png'),
+        PlanetBg: fs.readFileSync('static/img/PlanetBg.png'),
+        HillBg: fs.readFileSync('static/img/HillBg.jpg'),
+        indexJs: fs.readFileSync('compileJs/index.js'),
+        PlanetJs: fs.readFileSync('compileJs/components/Planet.js'),
+        memo_green: fs.readFileSync('static/img/memo_green.png'),
+        memo_yellow: fs.readFileSync('static/img/memo_yellow.png'),
+        memo_pink: fs.readFileSync('static/img/memo_pink.png')
     };
-    // readFileSync한 것들 객체로 묶어 둠. 
+    // readFileSync한 것들 객체로 묶어 둠.
     var resSet = function (statuscode, contentType, fileName) {
         res.writeHead(statuscode, { 'Content-Type': contentType });
         // res.write(fileName)
@@ -40,35 +43,44 @@ var server = http.createServer(function (req, res) {
     switch (method) {
         case 'GET':
             switch (_url) {
-                case "/":
-                    resSet(200, "text/html", File.main);
+                case '/':
+                    resSet(200, 'text/html', File.main);
                     break;
-                case "/static/css/index.css":
-                    resSet(200, "text/css", File.indexCss);
+                case '/static/css/index.css':
+                    resSet(200, 'text/css', File.indexCss);
                     break;
-                case "/static/img/geunwonPlanet.png":
-                    resSet(200, "image/png", File.PlanetImg);
+                case '/static/img/geunwonPlanet.png':
+                    resSet(200, 'image/png', File.PlanetImg);
                     break;
-                case "/static/img/universeBg.png":
-                    resSet(200, "image/png", File.universeImg);
+                case '/static/img/universeBg.png':
+                    resSet(200, 'image/png', File.universeImg);
                     break;
-                case "/compileJs/index.js":
-                    resSet(200, "text/javascript", File.indexJs);
+                case '/compileJs/index.js':
+                    resSet(200, 'text/javascript', File.indexJs);
                     break;
-                case "/Planet":
-                    resSet(200, "text/html", File.Planet);
+                case '/Planet':
+                    resSet(200, 'text/html', File.Planet);
                     break;
-                case "/static/css/Planet.css":
-                    resSet(200, "text/css", File.PlanetCss);
+                case '/static/css/Planet.css':
+                    resSet(200, 'text/css', File.PlanetCss);
                     break;
-                case "/static/img/PlanetBg.png":
-                    resSet(200, "image/png", File.PlanetBg);
+                case '/static/img/PlanetBg.png':
+                    resSet(200, 'image/png', File.PlanetBg);
                     break;
-                case "/static/img/HillBg.jpg":
-                    resSet(200, "image/png", File.HillBg);
+                case '/static/img/HillBg.jpg':
+                    resSet(200, 'image/png', File.HillBg);
                     break;
-                case "/compileJs/components/Planet.js":
-                    resSet(200, "text/javascript", File.PlanetJs);
+                case '/compileJs/components/Planet.js':
+                    resSet(200, 'text/javascript', File.PlanetJs);
+                    break;
+                case '/static/img/memo_green.png':
+                    resSet(200, 'image/png', File.memo_green);
+                    break;
+                case '/static/img/memo_yellow.png':
+                    resSet(200, 'image/png', File.memo_yellow);
+                    break;
+                case '/static/img/memo_pink.png':
+                    resSet(200, 'image/png', File.memo_pink);
                     break;
             }
             break;
--- app.ts
+++ app.ts
@@ -1,57 +1,63 @@
-import * as fs from "fs";
-import * as http from "http";
-
+import * as fs from 'fs';
+import * as http from 'http';
 
 // interface file {
 //   [key : string] : file
 // }
 
-
 interface readFlie {
   fileName: {
-    main : Buffer;
-    Planet :  Buffer;
-    indexCss :  Buffer;
-    PlanetCss :  Buffer;
-    PlanetImg :  Buffer;
-    universeImg : Buffer;
-    PlanetBg :  Buffer;
-    HillBg :  Buffer;
-    indexJs :  Buffer;
-    PlanetJs :  Buffer;
-  }
-} 
+    main: Buffer;
+    Planet: Buffer;
+    indexCss: Buffer;
+    PlanetCss: Buffer;
+    PlanetImg: Buffer;
+    universeImg: Buffer;
+    PlanetBg: Buffer;
+    HillBg: Buffer;
+    indexJs: Buffer;
+    PlanetJs: Buffer;
+    memo_green: Buffer;
+    memo_yellow: Buffer;
+    memo_pink: Buffer;
+  };
+}
 // 객체로 readFileSync 묶을 내용 interface로 타입 지정해줌.
 
 //함수 만들기, 객체화 시키기(코딩 테스트 했던파일에 해본 것 있음)
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
-
     const method = req.method;
     const _url = req.url;
-    // url 변수로 담아서 switch문 매개변수로 씀. 
+    // url 변수로 담아서 switch문 매개변수로 씀.
 
-    const File:readFlie['fileName'] = {
-      main : fs.readFileSync("public/index.html"),
-      Planet : fs.readFileSync("static/html/Planet.html"),
-      indexCss : fs.readFileSync("static/css/index.css"),
-      PlanetCss : fs.readFileSync("static/css/Planet.css"),
-      PlanetImg : fs.readFileSync("static/img/geunwonPlanet.png"),
-      universeImg : fs.readFileSync("static/img/universeBg.png"),
-      PlanetBg : fs.readFileSync("static/img/PlanetBg.png"),
-      HillBg : fs.readFileSync("static/img/HillBg.jpg"),
-      indexJs : fs.readFileSync("compileJs/index.js"),
-      PlanetJs : fs.readFileSync("compileJs/components/Planet.js")
-    } 
-    // readFileSync한 것들 객체로 묶어 둠. 
+    const File: readFlie['fileName'] = {
+      main: fs.readFileSync('public/index.html'),
+      Planet: fs.readFileSync('static/html/Planet.html'),
+      indexCss: fs.readFileSync('static/css/index.css'),
+      PlanetCss: fs.readFileSync('static/css/Planet.css'),
+      PlanetImg: fs.readFileSync('static/img/geunwonPlanet.png'),
+      universeImg: fs.readFileSync('static/img/universeBg.png'),
+      PlanetBg: fs.readFileSync('static/img/PlanetBg.png'),
+      HillBg: fs.readFileSync('static/img/HillBg.jpg'),
+      indexJs: fs.readFileSync('compileJs/index.js'),
+      PlanetJs: fs.readFileSync('compileJs/components/Planet.js'),
+      memo_green: fs.readFileSync('static/img/memo_green.png'),
+      memo_yellow: fs.readFileSync('static/img/memo_yellow.png'),
+      memo_pink: fs.readFileSync('static/img/memo_pink.png'),
+    };
+    // readFileSync한 것들 객체로 묶어 둠.
 
-    const resSet = (statuscode: number, contentType:string, fileName:Buffer) => {
-      res.writeHead(statuscode, {'Content-Type':contentType})
+    const resSet = (
+      statuscode: number,
+      contentType: string,
+      fileName: Buffer
+    ) => {
+      res.writeHead(statuscode, { 'Content-Type': contentType });
       // res.write(fileName)
-      res.end(fileName)
-    }
+      res.end(fileName);
+    };
     // 반복되는 부분 함수로 묶음.
-  
 
     // const main = fs.readFileSync("public/index.html");
 
@@ -73,53 +79,62 @@ const server = http.createServer(
 
     // const PlanetJs = fs.readFileSync("compileJs/components/Planet.js");
 
-
-
-    switch(method){
+    switch (method) {
       case 'GET':
+        switch (_url) {
+          case '/':
+            resSet(200, 'text/html', File.main);
+            break;
 
-        switch(_url){
-          case "/":
-            resSet(200, "text/html", File.main)
-          break;
+          case '/static/css/index.css':
+            resSet(200, 'text/css', File.indexCss);
+            break;
 
-          case "/static/css/index.css":
-            resSet(200, "text/css", File.indexCss)
-          break;
+          case '/static/img/geunwonPlanet.png':
+            resSet(200, 'image/png', File.PlanetImg);
+            break;
 
-          case "/static/img/geunwonPlanet.png":
-            resSet(200, "image/png", File.PlanetImg)
-          break;
+          case '/static/img/universeBg.png':
+            resSet(200, 'image/png', File.universeImg);
+            break;
 
-          case "/static/img/universeBg.png":
-            resSet(200, "image/png", File.universeImg)
-          break;
+          case '/compileJs/index.js':
+            resSet(200, 'text/javascript', File.indexJs);
+            break;
 
-          case "/compileJs/index.js":
-            resSet(200, "text/javascript", File.indexJs)
-          break;
+          case '/Planet':
+            resSet(200, 'text/html', File.Planet);
+            break;
 
-          case "/Planet":
-            resSet(200, "text/html", File.Planet)
-          break;
+          case '/static/css/Planet.css':
+            resSet(200, 'text/css', File.PlanetCss);
+            break;
 
-          case "/static/css/Planet.css":
-            resSet(200, "text/css", File.PlanetCss)
-          break;
+          case '/static/img/PlanetBg.png':
+            resSet(200, 'image/png', File.PlanetBg);
+            break;
 
-          case "/static/img/PlanetBg.png":
-            resSet(200, "image/png", File.PlanetBg)
-          break;
+          case '/static/img/HillBg.jpg':
+            resSet(200, 'image/png', File.HillBg);
+            break;
 
-          case "/static/img/HillBg.jpg":
-            resSet(200, "image/png", File.HillBg)
-          break;
+          case '/compileJs/components/Planet.js':
+            resSet(200, 'text/javascript', File.PlanetJs);
+            break;
 
-          case "/compileJs/components/Planet.js":
-            resSet(200, "text/javascript", File.PlanetJs)
-          break;
+          case '/static/img/memo_green.png':
+            resSet(200, 'image/png', File.memo_green);
+            break;
+
+          case '/static/img/memo_yellow.png':
+            resSet(200, 'image/png', File.memo_yellow);
+            break;
+
+          case '/static/img/memo_pink.png':
+            resSet(200, 'image/png', File.memo_pink);
+            break;
         }
-      break;
+        break;
     }
 
     // if (req.method === "GET") {
@@ -169,8 +184,6 @@ const server = http.createServer(
     // }
   }
 );
-
-
 
 server.listen(3050, (err?: ErrorCallback) => {
   if (err) throw err;
